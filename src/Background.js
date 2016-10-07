8bb005356ea9357205f77e8c95c108e8e81b5f64
Peter C <peter.chau.esq@gmail.com>
2016-10-07 03:16:03 +0000

    implemented background

---
+++ src/Background.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// import css
+import './Background.css';
+
+class Background extends Component {
+  render() {
+    return (
+      <div className="Background">
+        <div className="dot one"></div>
+        <div className="dot two"></div>
+      </div>
+    );
+  }
+}
+
+export default Background;
