9c466eed7b83035370f381a9f1039e4db9bcc6e6
Peter C <peter.chau.esq@gmail.com>
2016-10-22 20:56:01 +0000

    corrected mobile view bug and added a readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# laboratory one
+
+## development
+- `npm run dev`
+- `npm test`
+
+## deployment
+- `npm run build`
+- `npm run deploy`
--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,8 @@ class Header extends Component {
   render() {
     return (
       <div className="Header mv6-ns mv5">
-        <h1 className="f-subheadline-ns f1 system-serif ttu Logo">laboratory one</h1>
+        <h1 className="f-subheadline-ns f1 system-serif ttu Logo">laboratory</h1>
+        <h1 className="f-subheadline-ns f1 system-serif ttu Logo">one</h1>
       </div>
     );
   }
